da69850dfd097fc85da1097e2dfc972c5756e690
macs <macs@macss-MacBook-Pro.local>
2020-10-24 22:52:36 +0100

    added clear all fuction

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const form = document.querySelector('form');
 const unorderedList = document.querySelector('ul');
+const clearAll = document.querySelector('#clearAll');
 
+// After Window Loads, display whatever was saved in the previous session
+
+window.onload = () => {
+    for ( i = 0; i < localStorage.length; i++) {
+        const createList = document.createElement('LI');
+        let storedKeys = localStorage.key(i);
+        console.log(localStorage.getItem(storedKeys))
+        createList.innerHTML = localStorage.getItem(storedKeys)
+        unorderedList.appendChild(createList)
+    }
+
+    // hide clear button and Task display when There's nothing
+
+    if( localStorage.length < 1){
+        unorderedList.style.visibility = 'hidden';
+        clearAll.style.visibility = 'hidden';
+    }
+}
+
+    // Add User Input to To-Do list
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    unorderedList.style.visibility = 'visible';
+    
     const createList = document.createElement('LI');
     const input = document.querySelector('input');
+
+    // unhide the clear button and the Task display
+
+    unorderedList.style.visibility = 'visible';
+    clearAll.style.visibility = 'visible';
     
 
     // Save User's input to local storage
@@ -25,11 +51,12 @@ form.addEventListener('submit', e => {
 
 })
 
+clearAll.addEventListener('click', e => {
+    localStorage.clear();
+    unorderedList.style.visibility = 'hidden';
+    clearAll.style.visibility = 'hidden';
+    location.reload()
+})
 
-// for(i = 0; i < localStorage.length;i++){
-//     let storeKey = `input${localStorage.length}`;
-//     createList.innerHTML = localStorage.getItem('input0');
-//     unorderedList.appendChild(createList);
-//     console.log(createList.innerHTML)
-// }
+
 
